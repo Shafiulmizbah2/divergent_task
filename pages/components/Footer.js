34543a0372dd186e9df424689be2c7feb28ab051
Shafiul Azim <shafiulmizbah2@gmail.com>
2022-10-11 19:15:33 +0600

    initial project setup

---
+++ pages/components/Footer.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Footer = (props) => {
+  return <Container>Footer</Container>;
+};
+
+const Container = styled.footer`
+  background: ${({ theme }) => theme.black};
+`;
+
+export default Footer;
---
+++ theme.js
@@ -0,0 +1,8 @@
+export default {
+  text: "#333333",
+  black: "#000000",
+  white: "#ffffff",
+  primary: "#4F2DFF",
+  primaryLight: "#2D9CDB",
+  secondary: "#50B13F",
+};
